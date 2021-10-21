a85ca35c0f159e56743a500093327631a3eb4218
antoine <a0415@hotmail.fr>
2021-10-21 10:25:12 +0200

    session mentora sur page Index

---
+++ front/js/app.js
@@ -0,0 +1,37 @@
+function getProducts() {
+  fetch("http://localhost:3000/api/products")
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (data) {
+        createProducts(data)
+    })
+    .catch(function (err) {
+      // Une erreur est survenue
+    });
+}
+
+
+function createProduct(product) {
+  let productHTML = document.createElement("a");
+  productHTML.href = `./product.html?id=${product._id}`;
+  productHTML.innerHTML = `
+    <article>
+        <img src="${product.imageUrl}" alt="${product.altTxt}">
+        <h3 class="productName">${product.name}</h3>
+        <p class="productDescription">${product.description}</p>
+    </article>
+`;
+return productHTML;
+}
+
+function createProducts(products){
+    products.forEach(product => {
+        let productHTML = createProduct(product)
+        document.getElementById('items')
+                .appendChild(productHTML)
+    });
+}
+getProducts();
