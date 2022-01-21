757dcc7d6db766669456c5b3ddaf5b4647a88192
antoine <a0415@hotmail.fr>
2022-01-21 11:34:53 +0100

    POST vers api

--- front/js/cart.js
+++ front/js/cart.js
@@ -30,17 +30,17 @@ function createProduct(product) {
                                             </div>
                                               </article>
                                                 `;
-  productHTML.querySelector('.itemQuantity').addEventListener('change', function(){
+  productHTML.querySelector('.itemQuantity').addEventListener('change', function () {
     product.quantity = this.value
     let total = calculatePrice(product)
 
     productHTML.querySelector('.price').textContent = total
     displayTotal();
     updateCard(product);
-  })                                              
+  })
   return productHTML;
 }
-function updateCard(product){
+function updateCard(product) {
   let cardProducts = JSON.parse(localStorage.getItem("product"));
   let indexProduct = cardProducts.findIndex(element => element.color === product.color);
   let cardWithProductDelete = cardProducts.splice(indexProduct, 1);
@@ -68,7 +68,7 @@ function total() {
 function totalcartQuantity() {
   let totalQuantity = document.querySelectorAll('.itemQuantity')
   let totalCartQuantity = 0;
-    totalQuantity.forEach(product => {
+  totalQuantity.forEach(product => {
 
     totalCartQuantity = parseInt(product.value) + totalCartQuantity
   });
@@ -88,7 +88,7 @@ function displayTotal() {
 
 //-------------------------------------------------------------------
 //-------------------------------------------------------------------
-function calculatePrice(product){
+function calculatePrice(product) {
   let total = product.price * product.quantity
   return total
 }
@@ -111,9 +111,9 @@ function deleteLine(color) {
 function deleteProduct() {
   let deleteItems = document.querySelectorAll(".deleteItem")
   deleteItems.forEach(item => {
-    item.addEventListener('click', function(){
+    item.addEventListener('click', function () {
       deleteLine(this.getAttribute("data-color"))
-    });    
+    });
   })
 }
 
@@ -140,3 +140,66 @@ displayTotal();
 
 
 
+function postData(url = '/order', data = {}) {
+  const response = fetch(url, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  return response.json()
+}
+
+
+
+document.querySelector('#order').addEventListener('click', (e) => {
+  e.preventDefault();
+  let data = setData();
+  let response = postData('http://localhost:3000/api', data)
+  console.log(response)
+})
+function setData(){
+  let cardProducts = JSON.parse(localStorage.getItem("product"));
+  let products = []
+  cardProducts.forEach(product => {
+    products.push(product.id)
+  })
+  let data = {
+    contact: {
+      firstName: document.getElementById('firstName').value,
+      lastName: document.getElementById('lastName').value,
+      address: document.getElementById('address').value,
+      city: document.getElementById('city').value,
+      email: document.getElementById('email').value,
+    },
+    products: products
+  }
+  return data
+}
+
+
+function onPost(e) {
+  e.preventDefault();
+  let data = {
+    firstname: form.elements['firstname'].value,
+    lastName: form.elements['lastName'].value,
+    address: form.elements['address'].value,
+    city: form.elements['city'].value,
+    email: form.elements['email'].value
+  };
+
+  postData('back/controlleurs/product.js', data)
+    .then(data => {
+      alertElement.classlist.remove('success', 'error');
+      alertElement.innerHTML = data.message;
+      console.log(data);
+
+      if (data.success) {
+        alertElement.classlist.add('success');
+      }
+      else {
+        alertElement.classlist.add('error');
+      }
+    });
+}
