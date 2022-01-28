f131b322be8ac33313d2e1d4b1ce92216ce533df
antoine <a0415@hotmail.fr>
2022-01-28 15:55:15 +0100

    validation formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,3 @@
-
-
 // // // //---------------------------------------------------------------------
 // // // //Integration des produits sur la page panier
 // // // //---------------------------------------------------------------------
@@ -64,7 +62,6 @@ function total() {
 //-------------------------------------------------------------------
 //calcul quantité total
 //-------------------------------------------------------------------
-
 function totalcartQuantity() {
   let totalQuantity = document.querySelectorAll('.itemQuantity')
   let totalCartQuantity = 0;
@@ -77,15 +74,12 @@ function totalcartQuantity() {
 //-------------------------------------------------------------------
 //Ajout des résultats en bas de page(quantité et prix totale)
 //-------------------------------------------------------------------
-
 function displayTotal() {
   let totalPrice = document.getElementById('totalPrice')
   totalPrice.innerText = total();
   let totalQuantity = document.getElementById('totalQuantity')
   totalQuantity.textContent = totalcartQuantity();
 }
-
-
 //-------------------------------------------------------------------
 // Calcul du prix Total 
 //-------------------------------------------------------------------
@@ -96,7 +90,6 @@ function calculatePrice(product) {
 //-------------------------------------------------------------------
 // Rechercher l'élément du tableau qui a l'id et la couleur du noeud
 //-------------------------------------------------------------------
-
 function deleteLine(color) {
   let cardProducts = JSON.parse(localStorage.getItem("product"));
   let indexProduct = cardProducts.findIndex(element => element.color === color);
@@ -105,8 +98,6 @@ function deleteLine(color) {
   location.reload()
   console.log(cardWithProductDelete);
 }
-
-
 //-------------------------------------------------------------------
 //selection de la ligne pour la supprimer
 //------------------------------------------------------------------- 
@@ -138,50 +129,49 @@ function createProducts() {
 //envoie des données dans l'api
 //-------------------------------------------------
 
- async function postData(url = 'http://localhost:3000/api/products', data = {}) {
-  const response = await fetch(url, {
+async function postData(url = 'http://localhost:3000/api/products', data = {}) {
+  const response = await fetch(url, {   
     method: "POST",
     headers: {
       'Content-Type': 'application/json',
-
     },
     body: JSON.stringify(data)
   })
-  .then(response => response.json())
-  .then(data => {
-    let orderId = data.orderId
-    window.location.href ='confirmation.html?orderId=' + orderId
-    return data
-   
-  })
-  
+    .then(response => response.json())
+    .then(data => {
+      let orderId = data.orderId
+      console.log(orderId);
+      if (orderId !== undefined){    
+      window.location.href = 'confirmation.html?orderId=' + orderId
+      return data
+    }else{     
+    }
+    })
   return response
-  
 }
 //-------------------------------------
 //Ajout de l'ID dans l'url et sur la page de validation
 //-------------------------------------
 
 let searchParams = new URLSearchParams(window.location.search);
-
 if (searchParams.has('orderId')) {
   let orderId = searchParams.get('orderId')
   let orderNum = document.getElementById('orderId')
   orderNum.innerText = orderId
-  console.log(orderId)
+  console.log(orderNum)
 } else {
-  
-}
+ 
+};
 
-console.log(searchParams.has('orderId'));
-document.querySelector('#order').addEventListener('click', (e) => {
+document.querySelector('#form').addEventListener('submit', (e) => {
   e.preventDefault();
+  console.log('coucou');
   let data = setData();
-  console.log(data)
+  // console.log(data)
   let response = postData(url = "http://localhost:3000/api/products/order", data)
-  console.log(response)
-})
-validation();
+  // console.log(response)
+});
+
 
 
 function setData() {
@@ -212,103 +202,237 @@ function command() {
 createProducts();
 displayTotal();
 
+function test(){
+const form = document.querySelectorAll(
+  'input[type="text"], input[type="email"]');
+  let firstName, lastName, address, city, email;
+
+  let errorDisplay = (tag, message, valid) => {
+  let container = document.getElementById(tag)
+  let error =  document.querySelector("#" + tag + "ErrorMsg")
+  if(!valid){
+    container.classList.add('error');
+    error.textContent = message;
+  } else {
+    container.classList.remove('error');
+    error.textContent = message;
+  }
+};
+//----------------check firstName
+function firstNameCheck(value){
+  if (value.length <= 0 ){
+    errorDisplay('firstName', "veulliez remplir le champ");
+    firstName = null;
+  }
+  else if (!value.match(/^[A-Za-z]+((\s)?([A-Za-z])+)*$/)){
+    errorDisplay('firstName', "Format non valide");
+    firstName = null;
+  }
+  else{
+    errorDisplay('firstName',"", true);
+    firstName = value;
+  }
+};
+//---------------check LastName
+function lastNameCheck(value){
+  if (value.length <= 0 ){
+    errorDisplay('lastName', "veulliez remplir le champ");
+    lastName = null;
+  }
+  else if (!value.match(/^[A-Za-z]+((\s)?([A-Za-z])+)*$/)){
+    errorDisplay('lastName', "Format non valide");
+    lastName = null;
+  }
+  else{
+    errorDisplay('lastName',"", true);
+    lastName = value;
+  }
+};
+
+// //----------------------check address
+let addressCheck = (value) => {
+  if(value.length <= 0 ){
+    errorDisplay('address', "veulliez remplir le champ");
+    address = null;
+  }
+  else if (!value.match(/^[a-zA-Z0-9\s,.'-]{3,}$/)){
+    errorDisplay('address', "Format non valide");
+    address = null;
+  }
+  else{
+    errorDisplay('address',"", true);
+    address = value;
+  }
+};
+//-----------------check city
+let cityCheck = (value) => {
+  if(value.length <= 0 ){
+    errorDisplay('city', "veulliez remplir le champ");
+    city = null;
+  }
+  else if (!value.match(/^[A-Za-z]+((\s)?([A-Za-z])+)*$/)){
+    errorDisplay('city', "Format non valide");
+    city = null;
+  
+  }
+  else{
+    errorDisplay('city',"", true);
+    city = value;
+  }
+};
+//-----------------check Email
+let emailCheck = (value) => {
+  if(value.length <= 0 ){
+    errorDisplay('email', "veulliez remplir le champ");
+    email = null;
+    return false
+  }
+  else if (!value.match(/([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}(;|$))/)){
+    errorDisplay('email', "Format non valide");
+    email = null;
+    return false
+  }
+  else{
+    errorDisplay('email',"", true);
+    email = value;
+  }
+};
 
 
+form.forEach((name) => {
+  name.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "firstName":
+      firstNameCheck(e.target.value);
+      break;
+      case "lastName":
+        lastNameCheck(e.target.value);
+        break;
+        case "address":
+          addressCheck(e.target.value);
+          break;
+          case "city":
+            cityCheck(e.target.value);
+            break;
+              case "email":
+            emailCheck(e.target.value);
+              break;
+                default:
+                  null;
+    }
+  });  
+});
+};
+
+test();
+
+
+// console.log(validation());
 
 //-----------------------------------------
 //Validation du formulaire
 //-----------------------------------------
 
-function validation(){
-  let valid = document.getElementById('order');
+// function validation(){
+//   let valid = document.getElementById('order');
 
-  //----validation prenom-------
-  let firstName = document.getElementById('firstName');
-  let firstNameErr = document.getElementById('firstNameErrorMsg');
-  let firstNameReg = /^[A-Za-z]+((\s)?([A-Za-z])+)*$/;
-  valid.addEventListener('click', firstName_valid)
-    function firstName_valid(e){
-      if (firstName.validity.valueMissing) {
-        e.preventDefault();
-        firstNameErr.textContent = 'Veulliez renseigner le Prenom';
-        firstNameErr.style.color = 'red';
-      }else if (!firstNameReg.test(firstName.value)){
-        e.preventDefault();
-        firstNameErr.textContent = 'Format incorrect';
-        firstNameErr.style.color = 'orange';
-      }else{
 
-      }
+//   //----validation prenom-------
+//   let firstName = document.getElementById('firstName');
+//   let firstNameErr = document.getElementById('firstNameErrorMsg');
+//   let firstNameReg = /^[A-Za-z]+((\s)?([A-Za-z])+)*$/;
+ 
+//   valid.addEventListener('click', firstName_valid)
+  
+//      function firstName_valid(e){
+//       if (firstName.validity.valueMissing) {
+//         e.preventDefault()
+//         firstNameErr.textContent = 'Veulliez renseigner le Prenom';
+//         firstNameErr.style.color = 'red';
+//         return false
+//       }else if (!firstNameReg.test(firstName.value)){
+//         e.preventDefault()
+//         firstNameErr.textContent = 'Format incorrect';
+//         firstNameErr.style.color = 'orange';
+//         return false
+//       }else{
+//       }
+//     }
 
-    }
 
-   //---Validation Nom de famille-------
-  let lastName = document.getElementById('lastName');
-  let lastNameErr = document.getElementById('lastNameErrorMsg');
-  let lastNameReg = /^[A-Za-z]+((\s)?([A-Za-z])+)*$/;
-  valid.addEventListener('click', lastName_valid)
-  function lastName_valid(e){
-    if (lastName.validity.valueMissing) {
-      e.preventDefault();
-      lastNameErr.textContent = 'Veulliez renseigner le Nom de famille';
-      lastNameErr.style.color = 'red';
-    }else if (!lastNameReg.test(lastName.value)){
-      e.preventDefault();
-      lastNameErr.textContent = 'Format incorrect';
-      lastNameErr.style.color = 'orange';
-    }else{
 
-    }
-  }
-    //---Validation adresse----------
-    let address = document.getElementById('address');
-    let addressErr = document.getElementById('addressErrorMsg');
-    let addressReg = /^[a-zA-Z0-9\s,'-]*$/;
-    valid.addEventListener('click', address_valid)
-    function address_valid(e){
-      if (address.validity.valueMissing) {
-        e.preventDefault();
-        addressErr.textContent = 'Veulliez renseigner votre adresse';
-        addressErr.style.color = 'red';
-      }else if (!addressReg.test(lastName.value)){
-        e.preventDefault();
-        addressErr.style.color = 'orange';
-        addressErr.textContent = 'Format incorrect';
-      }else{
-      }
-    }
-        //---Validation Ville----------
-        let city = document.getElementById('city');
-        let cityErr = document.getElementById('cityErrorMsg');
-        let cityReg = /^[A-Za-z]+((\s)?([A-Za-z])+)*$/;
-        valid.addEventListener('click', city_valid)
-        function city_valid(e){
-          if (city.validity.valueMissing) {
-            e.preventDefault();
-            cityErr.textContent = 'Veulliez renseigner votre ville';
-            cityErr.style.color = 'red';
-          }else if (!cityReg.test(city.value)){
-            e.preventDefault();
-            cityErr.style.color = 'orange';
-            cityErr.textContent = 'Format incorrect';
-          }else{
-          }
-        }
-          //---Validation Email----------
-          let email = document.getElementById('email');
-          let emailErr = document.getElementById('emailErrorMsg');
-          let emailReg = /([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}(;|$))/;
-          valid.addEventListener('click', email_valid)
-          function email_valid(e){
-            if (email.validity.valueMissing) {
-              e.preventDefault();
-              emailErr.textContent = 'Veulliez renseigner votre Email';
-              emailErr.style.color = 'red';
-            }else if (!emailReg.test(email.value)){
-              e.preventDefault();
-              emailErr.style.color = 'orange';
-              emailErr.textContent = 'Format incorrect';
-            }else{
-            }
-          }
-  };
+
+//   //  //---Validation Nom de famille-------
+//   // let lastName = document.getElementById('lastName');
+//   // let lastNameErr = document.getElementById('lastNameErrorMsg');
+//   // let lastNameReg = /^[A-Za-z]+((\s)?([A-Za-z])+)*$/;
+//   // valid.addEventListener('click', lastName_valid)
+//   // function lastName_valid(e){
+//   //   if (lastName.validity.valueMissing) {
+//   //     e.preventDefault();
+//   //     lastNameErr.textContent = 'Veulliez renseigner le Nom de famille';
+//   //     lastNameErr.style.color = 'red';
+//   //   }else if (!lastNameReg.test(lastName.value)){
+//   //     e.preventDefault();
+//   //     lastNameErr.textContent = 'Format incorrect';
+//   //     lastNameErr.style.color = 'orange';
+//   //   }else{
+
+//   //   }
+//   // }
+//   //   //---Validation adresse----------
+//   //   let address = document.getElementById('address');
+//   //   let addressErr = document.getElementById('addressErrorMsg');
+//   //   let addressReg = /^[a-zA-Z0-9\s,'-]*$/;
+//   //   valid.addEventListener('click', address_valid)
+//   //   function address_valid(e){
+//   //     if (address.validity.valueMissing) {
+//   //       e.preventDefault();
+//   //       addressErr.textContent = 'Veulliez renseigner votre adresse';
+//   //       addressErr.style.color = 'red';
+//   //     }else if (!addressReg.test(lastName.value)){
+//   //       e.preventDefault();
+//   //       addressErr.style.color = 'orange';
+//   //       addressErr.textContent = 'Format incorrect';
+//   //     }else{
+//   //       addressVerif = true
+//   //     }
+//   //   }
+//   //       //---Validation Ville----------
+//   //       let city = document.getElementById('city');
+//   //       let cityErr = document.getElementById('cityErrorMsg');
+//   //       let cityReg = /^[A-Za-z]+((\s)?([A-Za-z])+)*$/;
+//   //       valid.addEventListener('click', city_valid)
+//   //       function city_valid(e){
+//   //         if (city.validity.valueMissing) {
+//   //           e.preventDefault();
+//   //           cityErr.textContent = 'Veulliez renseigner votre ville';
+//   //           cityErr.style.color = 'red';
+//   //         }else if (!cityReg.test(city.value)){
+//   //           e.preventDefault();
+//   //           cityErr.style.color = 'orange';
+//   //           cityErr.textContent = 'Format incorrect';
+//   //         }else{
+
+//   //         }
+//   //       }
+//   //         //---Validation Email----------
+//   //         let email = document.getElementById('email');
+//   //         let emailErr = document.getElementById('emailErrorMsg');
+//   //         let emailReg = /([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}(;|$))/;
+//   //         valid.addEventListener('click', email_valid)
+//   //         function email_valid(e){
+//   //           if (email.validity.valueMissing) {
+//   //             e.preventDefault();
+//   //             emailErr.textContent = 'Veulliez renseigner votre Email';
+//   //             emailErr.style.color = 'red';
+//   //           }else if (!emailReg.test(email.value)){
+//   //             e.preventDefault();
+//   //             emailErr.style.color = 'orange';
+//   //             emailErr.textContent = 'Format incorrect';
+//   //           }else{
+
+//   //           }
+//   //         }
+//   };
+ 
